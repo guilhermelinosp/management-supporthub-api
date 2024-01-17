5bddfa97bdb79fd4ecace6ab0aa282fcab5a239b
Guilherme Lino <guilhermelinosp@gmail.com>
2024-01-17 19:28:25 -0300

    feat: structuring the project

--- Source/Customer/Customer.Management.SupportHub.API/Program.cs
+++ Source/Customer/Customer.Management.SupportHub.API/Program.cs
@@ -1,5 +1,3 @@
-using Customer.Management.SupportHub.API;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -17,25 +15,4 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-	"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-	{
-		var forecast = Enumerable.Range(1, 5).Select(index =>
-				new WeatherForecast
-				(
-					DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-					Random.Shared.Next(-20, 55),
-					summaries[Random.Shared.Next(summaries.Length)]
-				))
-			.ToArray();
-		return forecast;
-	})
-	.WithName("GetWeatherForecast")
-	.WithOpenApi();
-
 app.Run();
--- Source/Customer/Customer.Management.SupportHub.API/WeatherForecast.cs
+++
@@ -1,6 +0,0 @@
-namespace Customer.Management.SupportHub.API;
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
